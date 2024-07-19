f0730c217024628bfd205cc5a36402a49766820b
Dev Aanand Bachhar <bachhardevaanand@gmail.com>
2024-07-19 19:32:19 +0545

    this is a slice method of array.

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,9 +1,24 @@
 // '#  Array.join() Method
 
-const elements = ['Namaste','Nepal']
-console.log(elements.join());
-console.log(elements.join(''));
-console.log(elements.join('-'));
+// const elements = ['Namaste','Nepal']
+// console.log(elements.join());
+// console.log(elements.join(''));
+// console.log(elements.join('-'));
+
+// # Array slice() method
+
+const animals = ['ant','bison','camel','duck','elephant'];
+console.log(animals.slice());
+console.log();
+console.log(animals.slice(2));
+console.log();
+console.log(animals.slice(1,3));
+console.log();
+console.log(animals.slice(-4));
+console.log(animals.slice(1,-1));
+
+
+
 
 
 
