afb527ec10fc7345af5980b51ae71debf55a7f1c
Dev Aanand Bachhar <bachhardevaanand@gmail.com>
2024-07-19 19:41:26 +0545

    This is a indexOf method of arrays.

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -7,18 +7,24 @@
 
 // # Array slice() method
 
-const animals = ['ant','bison','camel','duck','elephant'];
-console.log(animals.slice());
-console.log();
-console.log(animals.slice(2));
-console.log();
-console.log(animals.slice(1,3));
-console.log();
-console.log(animals.slice(-4));
-console.log(animals.slice(1,-1));
+// const animals = ['ant','bison','camel','duck','elephant'];
+// console.log(animals.slice());
+// console.log();
+// console.log(animals.slice(2));
+// console.log();
+// console.log(animals.slice(1,3));
+// console.log();
+// console.log(animals.slice(-4));
+// console.log(animals.slice(1,-1));
 
 
+// # array method  indexOf()
 
+const beasts = ['ant','bison','camel','duck','bison']
+
+console.log(beasts.indexOf('bison'));
+console.log(beasts.indexOf('bison',2));
+console.log(beasts.indexOf('giraffe'));
 
 
 
