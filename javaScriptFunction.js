b495de00a3814cc634137684272a52d8bd339194
Dev Aanand Bachhar <bachhardevaanand@gmail.com>
2024-07-22 23:14:43 +0545

    this is a function of javaScript .

---
+++ javaScriptFunction.js
@@ -0,0 +1,32 @@
+// function myfunction(){
+//     console.log("javaScript Function");
+// }
+// myfunction();
+
+// function define garna function keyword used garnu parxa ra function ko name liknu parxa.
+// function ko name pachi parenthesis declare garnu parxa.
+
+// function myfunction2(msg){
+//     console.log(msg);
+// }
+// myfunction2("Hello JavaScript function");
+
+// function declare garne bela ma variable pass gareko lai parameters bhanxa.
+// function call garda variable pass gareko lai arguments bhanxa.
+
+// function sum(x,y){
+//     s = x*y;
+//     return s;
+// }
+// sum(5,8)
+// console.log(s);
+
+// arrow function of javaScript.
+
+// above sum function in arrow funtion 
+
+const arrowSum=(x,y)=>{
+    return x*y;
+}
+
+console.log(arrowSum(9,8));
